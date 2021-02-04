85c03d635a1e2850885d2110970f66b6630be2da
Ade Yusuf Yaumul Isnain <yaumulisnain@gmail.com>
2021-02-04 08:57:05 +0700

    Add cache when get music chart

--- src/v1/repository/musicRepository.go
+++ src/v1/repository/musicRepository.go
@@ -1,8 +1,11 @@
 package repository
 
 import (
+	"account-service/src/core"
 	h "account-service/src/helper"
 	"account-service/src/v1/model"
+	"encoding/json"
+	"fmt"
 )
 
 func FetchChartCount() (int, error) {
@@ -22,10 +25,12 @@ func FetchChartCount() (int, error) {
 
 func FetchChartWithPaginate(paginationModel map[string][]string) ([]model.Music, int, int, error) {
 	var (
-		music []model.Music
-		err   error
-		limit int
-		page  int
+		music      []model.Music
+		musicRedis string
+		redisData  []byte
+		err        error
+		limit      int
+		page       int
 	)
 	db := getDB()
 
@@ -35,7 +40,31 @@ func FetchChartWithPaginate(paginationModel map[string][]string) ([]model.Music,
 		return music, limit, page, err
 	}
 
+	redisKey := fmt.Sprintf("music_%d_%d", page, limit)
+
+	musicRedis, err = core.RedisGet(redisKey)
+
+	if err == nil {
+		if err := json.Unmarshal([]byte(musicRedis), &music); err == nil {
+			return music, limit, page, nil
+		}
+	}
+
 	if err := db.Find(&music).Order("position ASC", true).Error; err != nil {
+		return music, limit, page, err
+	}
+
+	redisData, err = json.Marshal(music)
+
+	if err != nil {
+		return music, limit, page, err
+	}
+
+	if err = core.RedisSet(redisKey, string(redisData)); err != nil {
+		return music, limit, page, err
+	}
+
+	if err = core.RedisSetExpire(redisKey, 3600); err != nil {
 		return music, limit, page, err
 	}
 
