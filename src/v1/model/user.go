8202545a39423e8902f095bc94af681c23407d72
Ade Yusuf Yaumul Isnain <yaumulisnain@gmail.com>
2021-02-04 09:21:48 +0700

    Add validation for gender and user name

--- src/v1/model/user.go
+++ src/v1/model/user.go
@@ -4,10 +4,10 @@ import "time"
 
 type User struct {
 	ID        int32      `json:"id" gorm:"primary_key;AUTO_INCREMENT;unique_index"`
-	UserName  string     `json:"user_name" validate:"min=3,max=50,required" gorm:"not_null;unique_index"`
+	UserName  string     `json:"user_name" validate:"min=3,max=50,alphanum,required" gorm:"not_null;unique_index"`
 	FullName  string     `json:"full_name" validate:"min=3,required" gorm:"not_null"`
 	Password  string     `json:"password" validate:"min=5,required" gorm:"not_null"`
-	Gender    string     `json:"gender,omitempty" gorm:"default:null"`
+	Gender    string     `json:"gender,omitempty" validate:"eq=M|eq=F|eq=" gorm:"default:null"`
 	Hobby     string     `json:"hobby,omitempty" gorm:"default:null"`
 	Address   string     `json:"address,omitempty" gorm:"default:null"`
 	CreatedAt time.Time  `json:"created_at"`
